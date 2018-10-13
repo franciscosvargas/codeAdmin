d98506cf12ddc710cb7e9587df586f6e98de31b0
Francisco Alisson Vargas <falisson.sv@gmail.com>
2018-10-13 12:15:22 -0300

    Login form added

--- js/code101.js
+++ js/code101.js
@@ -1,5 +1,14 @@
 drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector(".mdc-drawer"));
 
+chamalogin();
+
+function chamalogin(){
+	$('.mdc-drawer__content').css('opacity',0);
+	$("#pageContent").load("login.html"); 
+
+}
+
 $('.mdc-list-item').focus(function(){
 	this.blur();
-})
+});
+
