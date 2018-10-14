43cb3e061a0873fe1c8032d0f5088f95da629b57
Francisco Alisson Vargas <falisson.sv@gmail.com>
2018-10-14 16:49:08 -0300

    Suggestions page completed

--- js/code101.js
+++ js/code101.js
@@ -1,13 +1,12 @@
 drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector(".mdc-drawer"));
 
+$('.mdc-list-item').focus(function(){
+	this.blur();
+});
 
-window.onload = function() {
-	initApp();
-  };
+// ################ FIREBASE USER CONFIGS #######################
 
-// #### FIREBASE LOGIN SERVICES #######
-
-//SIGN IN USER
+//  SIGN IN USER 
 function login(){
 	let email = $("#userEmail").val();
 	let password = $("#userPassword").val();
@@ -17,18 +16,18 @@ function login(){
 	  });
 }
 
-// VERIFY IF USER ARE LOGGED
+// VERIFY IF USER IS LOGGED
 function initApp(){
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			// User is signed in.
-			$('.mdc-drawer__content').css('opacity',1);
-			$("#pageContent").load("dashboard.html");
 			setUserHeader();
-
+			$('.mdc-drawer__content').css('opacity',1);
+			$("#pageContent").load("tabs/dashboard.html");
+			
 		} else {
 			$('.mdc-drawer__content').css('opacity',0);
-			$("#pageContent").load("login.html"); 
+			$("#pageContent").load("tabs/login.html"); 
 		}
 	});
 }
@@ -50,6 +49,84 @@ function setUserHeader(){
 	
 }
 
-$('.mdc-list-item').focus(function(){
-	this.blur();
+// ################ SUGGESTIONS.HTML FUNCTIONS ##################
+
+function listSuggestions(){
+	let ref = firebase.database().ref('sugestions/');
+		ref.on('child_added', function(data) {
+		let commandName = data.val().command;
+		let languageName = data.val().language;
+		$('#my-list').append(" <li class='mdc-list-item suggestionItem  mdc-list--avatar-list' id='"+commandName+"' onclick='viewSuggestion(this.id);'> <span class='mdc-list-item__graphic' role='presentation'> <i class='material-icons' aria-hidden='true'>message</i> </span> <span class='mdc-list-item__text'> <span class='mdc-list-item__primary-text'>"+commandName+"</span> <span class='mdc-list-item__secondary-text'>"+languageName+"</span> </span> </li>").fadeIn(300);
+	});
+
+	ref.on('child_removed', function(data) {
+		$("#"+ data.val().command).remove().fadeOut(300);;
+	});
+}
+
+var dataRef = [];
+
+function viewSuggestion(cID){
+	let count = 0;
+	let ref = firebase.database().ref('sugestions/'+ cID);
+	ref.once('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+		   dataRef[count] = childSnapshot.val();
+		   count++;
+		});
+	});
+
+	$("#editCommandName").text(dataRef[0]);
+	$("#editLanguageName").text(dataRef[2]);
+	$("#editMessage").text(dataRef[3]);
+	$(".suggestionEdit").fadeIn(300);
+	return 0;
+}
+
+function deleteSuggestion(){
+	cID = $("#editCommandName").text();
+	let ref = firebase.database().ref('sugestions/' + cID);
+	$('.suggestionEdit').fadeOut(300);
+	ref.remove();
+	showSnackBar('Sugestão deletada');
+}
+
+
+function addSuggestion(){
+	cID = $("#editCommandName").text();
+	let ref = firebase.database().ref('sugestions/' + cID);
+	$('.suggestionEdit').fadeOut(300);
+	$.ajax({
+		type: "post",
+		url: "https://us-central1-code101-b884a.cloudfunctions.net/sugestaoAdicionada",
+		data: {commandName: dataRef[0], userEmail: dataRef[1]}
+	});
+	ref.remove();
+	showSnackBar("Confirmação de adição enviada");
+}
+$('.buttonEditSuggestion').mouseenter(function(){
+	$('.actionEditIndicator').css('display','block');
+	
+	$('.actionEditIndicator').html('<div class="mdc-chip__text">'+$(this).attr('name')+'</div>');
 });
+
+$('.buttonEditSuggestion').mouseleave(function(){
+	$('.actionEditIndicator').css('display','none');
+});
+
+
+
+// FUNCTIONS FOR ALL THE PAGES
+
+function showSnackBar(message){
+	const snackbar = mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar'));
+	const dataObj = {
+		message: message,
+		actionText: 'OK',
+		actionHandler: function () {
+			console.log('my cool function');
+		}
+	};
+snackbar.show(dataObj);
+}
+
